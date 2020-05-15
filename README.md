441e62f279ebe5fac3b0d7eb68e3502309fd8533
MichaelYanNJU <171240518@smail.nju.edu.cn>
2020-05-15 09:38:58 +0800

    Add README Add generation of while.

--- README.md
+++ README.md
@@ -10,4 +10,12 @@ To start testing, type
     ./run.sh
 
 (you need to comment out outputs in irsim:main.cc, because this run.sh uses a naive diff.)
+
 (All test data that you're wrong will be copied into tests/)
+
+### Grammers
+Grammers that will be tested
+- Arithmetic without division (Because divided by zero is hard to solve)
+- declare and airhtmetics of array.
+- if and if-else.
+- while
--- generator.c
+++ generator.c
@@ -4,6 +4,8 @@
 
 int no;
 void expr();
+void stmt();
+void stmts(int);
 
 void a(int i) {
     if(i > 4) {
@@ -61,42 +63,57 @@ void expr() {
         singleton();
     }
 }
+void stmt_return() {
+    printf("    return "); expr(); printf(";\n");
+}
+void stmt_if() {
+    printf("    if(");
+    parent_expr();
+    if((rand()&3) == 0) {
+        static const char* relops[] = {
+            "<=", ">=", "==", "!=", ">", "<"
+        };
+        printf(" %s ", relops[rand()%6]);
+        parent_expr();
+    }
+    printf(") {\n");
+    stmts(2);
+    printf("    }");
+    if(rand()&1) {
+        printf(" else {\n");
+        stmt(2);
+        printf("    }");
+    }
+    printf("\n");
+}
+void stmt_while() {
+    printf("    i%d = 0;\n", no);
+    printf("    while(i%d < %d) {\n", no, 1 + (rand()&3));
+    printf("    i%d = i%d + 1;\n", no, no);
+    stmts(2);
+    printf("    }\n");
+}
 void stmt() {
     switch(rand()&15) {
         case 0: {
-                    int flag = rand()&3;
-                    /*
-                    if(flag & 2) {
-                        printf("    while(");
-                    } else {
-                        printf("    if(");
-                    }
-                    */
-                    printf("    if(");
-                    parent_expr();
-                    if((rand()&3) == 0) {
-                        static const char* relops[] = {
-                            "<=", ">=", "==", "!=", ">", "<"
-                        };
-                        printf(" %s ", relops[rand()%6]);
-                        parent_expr();
-                    }
-                    printf(") {\n");
-                    stmt();
-                    printf("    }");
-                    if(flag == 1) {
-                        printf(" else {\n");
-                        stmt();
-                        printf("    }");
-                    }
-                    printf("\n");
-                    break;
+                    stmt_if();
+                    return;
                 }
         case 1: {
                     printf("    write(");
-                    ra();
+                    expr();
                     printf(");\n");
+                    return;
                 }
+        case 2:
+                stmt_while();
+                return;
+        case 14:
+                if((rand()&3) == 0) {
+                    stmt_return();
+                    return;
+                }
+                //fall
         default:
                 printf("    ");
                 ra();
@@ -105,17 +122,22 @@ void stmt() {
                 printf(";\n");
     }
 }
+void stmts(int n) {
+    for(int i = 0; i < n || ((rand() & 1) && i < 2 * n); ++i) {
+        stmt();
+    }
+}
 void gen_func() {
     printf("int f%d(int a%d1, int a%d2) {\n", no, no, no);
     printf("    int a%d3 = %d;\n", no, rand()&15);
     printf("    int a%d4 = %d;\n", no, rand()&15);
     printf("    int a%d5[2];\n", no);
+    printf("    int i%d;\n", no);
     printf("    a%d5[0] = 17173;\n", no);
     printf("    a%d5[1] = 4399;\n", no);
-    for(int i = 0; i < 5 || (rand() & 3); ++i) {
-        stmt();
-    }
+    stmts(5);
     printf("    return "); expr(); printf(";\n");
+    stmt_return();
     printf("}\n");
 }
 void gen_main() {
@@ -125,12 +147,11 @@ void gen_main() {
     printf("    int a%d3 = %d;\n", no, rand()&15);
     printf("    int a%d4 = %d;\n", no, rand()&15);
     printf("    int a%d5[2];\n", no);
+    printf("    int i%d;\n", no);
     printf("    a%d5[0] = 10151;\n", no);
     printf("    a%d5[1] = 98808;\n", no);
-    for(int i = 0; i < 5 || (rand() & 3); ++i) {
-        stmt();
-    }
-    printf("    return "); expr(); printf(";\n");
+    stmts(5);
+    stmt_return();
     printf("}\n");
 }
 int main() {
